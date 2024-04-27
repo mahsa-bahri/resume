66b879998456ee1c7cefa70b61063a3641efd074
Mahsa_bhr <mahsabahri79@gmail.com>
2024-04-27 10:38:29 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import data from './data.json'
+import './App.css';
+import { SocialIcon } from 'react-social-icons';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navigation'/>
+      <div className='fullpage'>
+        <h1>
+          {data.title}
+        </h1>
+        <h2>
+          {data.subtitle}
+        </h2>
+        {
+          Object.keys(data.links).map((keys)=>{
+            return <SocialIcon url={data.links[keys]}/>;
+          })
+        }
+        <h3>
+          {data.sections[0].title}
+        </h3>
+        <p>
+          {data.sections[0].items[0].content}
+        </p>
+      </div>
+      <div className='fullpage'/>
+      <div className='fullpage'/>
+
+    </div>
+  );
+}
+
+export default App;
